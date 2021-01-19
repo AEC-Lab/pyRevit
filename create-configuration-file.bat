43e9a03988a4d04cf47387a7767874ddae375aac
mpothier <39810086+mpothier@users.noreply.github.com>
2021-01-19 11:38:14 -0500

    Updated config file batch script

--- create-configuration-file.bat
+++ create-configuration-file.bat
@@ -1,63 +1,22 @@
+REM set critical config file variables (e.g. extensions)
+REM remaining config values will be set to defaults by pyRevit
+
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
+
+REM variables
 set parentpath=%AppData%\pyRevit
-set filename=%parentpath%\pyRevit_config.ini
+set filename="%parentpath%\pyRevit_config.ini"
+set extensionpath=%AppData%\pyRevitExtensions
+set extensionpath=%extensionpath:\=\\%
 
-if not exist %parentpath%\ mkdir %parentpath%
-
-echo.>%filename%
-
-echo [core]>%filename%
-echo checkupdates = False>>%filename%
-echo verbose = True>>%filename%
-echo debug = False>>%filename%
-echo filelogging = True>>%filename%
-echo startuplogtimeout = 10>>%filename%
-
-set pa=%APPDATA%\pyRevitExtensions
-set pa=%pa:\=\\%
-
-echo userextensions = ['%pa%']>>%filename%
-echo compilecsharp = True>>%filename%
-echo compilevb = True>>%filename%
-echo loadbeta = False>>%filename%
-echo rocketmode = False>>%filename%
-
-set pb=%APPDATA%\pyRevit\pyRevit-v45\pyRevit\pyrevitlib\pyrevit\output\outputstyles.css
-set pb=%pb:\=\\%
-
-echo outputstylesheet = %pb%>>%filename%
-echo bincache = True>>%filename%
-echo minhostdrivefreespace = 0>>%filename%
-echo requiredhostbuild = 0>>%filename%
-
-echo.>>%filename%
-
-echo [usagelogging]>>%filename%
-echo active = TRUE>>%filename%
-echo logfilepath = >>%filename%
-REM echo logserverurl = 
-
-echo.>>%filename%
-
-echo [pyRevitDevTools.extension]>>%filename%
-echo disabled = True>>%filename%
-echo private_repo = True>>%filename%
-echo username = >>%filename%
-echo password = >>%filename%
-
-echo.>>%filename%
-
-echo [pyRevitTools.extension]>>%filename%
-echo disabled = False>>%filename%
-echo private_repo = True>>%filename%
-echo username = >>%filename%
-echo password = >>%filename%
-
-echo.>>%filename%
-
-echo [pyRevitTutor.extension]>>%filename%
-echo disabled = True>>%filename%
-echo private_repo = True>>%filename%
-echo username = >>%filename%
-echo password = >>%filename%
+if not exist "%parentpath%" mkdir "%parentpath%""
+if exist %filename% (
+    REM keep other existing config intact while overwriting userextensions
+    echo.>>%filename%
+    echo [core]>>%filename%
+    echo userextensions = ["%extensionpath%"]>>%filename%
+) else (
+    echo [core]>%filename%
+    echo userextensions = ["%extensionpath%"]>>%filename%
+)
